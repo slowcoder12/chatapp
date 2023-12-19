71d3010cd585ec3319a3446f936456b62c458997
slowcoder12 <132445668+slowcoder12@users.noreply.github.com>
2023-12-19 08:36:51 +0530

    login front end

---
+++ public/js/login.js
@@ -0,0 +1,27 @@
+const loginForm = document.querySelector("#form");
+
+loginForm.addEventListener("submit", async function (event) {
+  event.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const loginUser = { email, password };
+  try {
+    const response = await axios.post(`/login`, loginUser);
+    if (response.status === 200) {
+      console.log(response);
+
+      alert("user logged in successfully");
+      console.log(response.data.token);
+    }
+  } catch (err) {
+    if (err.response) {
+      if (err.response.status === 401) {
+        alert(err.response.data.message);
+      } else if (err.response.status === 404) {
+        alert(err.response.data.message);
+      }
+    }
+  }
+});
